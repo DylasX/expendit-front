99e94f1848febfb26bd220fe4f019eab49839188
Miguel Sierra <miguel-sg01@hotmail.com>
2024-08-03 20:14:28 -0500

    feat: add persistor

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,25 @@ import AppRouter from '@/shared/router/Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+import { persistQueryClient } from '@tanstack/react-query-persist-client';
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false,
+      gcTime: 1000 * 60 * 60 * 24,
     },
   },
+});
+
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
+
+persistQueryClient({
+  queryClient,
+  persister: localStoragePersister,
 });
 
 function App() {
