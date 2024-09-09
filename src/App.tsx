36c6e80c4f63a1baab7415f7f15c59cc6daf8e0f
Miguel Sierra <miguel-sg01@hotmail.com>
2024-09-08 20:36:15 -0500

    feat: Update VITE_API_URL in .env.production and add StatusBar configuration in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,13 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from '@/shared/client/queryClient';
+import React from 'react';
+import { StatusBar } from '@capacitor/status-bar';
 
 function App() {
+  React.useEffect(() => {
+    StatusBar.setBackgroundColor({ color: '#27272a' });
+  }, []);
   return (
     <>
       <QueryClientProvider client={queryClient}>
