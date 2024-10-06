e9dfc1a952937d145fbfa0eaf81ff2a92752bbeb
Miguel Sierra <miguel-sg01@hotmail.com>
2024-10-06 18:14:42 -0500

    fix: height solution

--- src/pages/invitations/Invitations.tsx
+++ src/pages/invitations/Invitations.tsx
@@ -154,7 +154,7 @@ const Invitations: React.FC = () => {
   };
 
   return (
-    <div>
+    <>
       <Header
         owesYou={
           user?.myCredit?.reduce(
@@ -172,7 +172,7 @@ const Invitations: React.FC = () => {
       {isLoading ? (
         <Loader />
       ) : (
-        <section className='flex flex-col bg-zinc-500 bg-opacity-10 rounded-2xl w-full min-h-[80vh] animate-fade-up animate-duration-300'>
+        <section className='flex flex-col bg-zinc-500 bg-opacity-10 rounded-2xl w-full min-h-[70%] animate-fade-up animate-duration-300'>
           <div className='p-5 text-lg font-semibold text-left w-full mb-5 text-gray-50'>
             <div className='flex flex-row justify-between'>
               <h2>Invitations</h2>
@@ -236,7 +236,7 @@ const Invitations: React.FC = () => {
       <Drawer isFullScreen={true} open={open} onClose={onClose}>
         <InvitationForm onClose={onClose} />
       </Drawer>
-    </div>
+    </>
   );
 };
 
